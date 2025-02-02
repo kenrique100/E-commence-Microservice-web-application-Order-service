172dd26145dc541b466821173c3818d73b63361f
kenrique100 <ngwakenri2016@gmail.com>
2025-02-02 05:28:22 -0500

    exceptions

--- src/main/java/com/akentech/microservices/order/exception/ErrorDetails.java
+++ src/main/java/com/akentech/microservices/order/exception/ErrorDetails.java
@@ -1,7 +1,5 @@
 package com.akentech.microservices.order.exception;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
 public record ErrorDetails(LocalDateTime timestamp, String message, String details) {
