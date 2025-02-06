ea9ec539449f0aced19dd9d63e45f18ab7675340
kenrique100 <ngwakenri2016@gmail.com>
2025-02-06 13:40:16 -0500

    category removed

--- src/main/java/com/akentech/microservices/order/client/InventoryClient.java
+++ src/main/java/com/akentech/microservices/order/client/InventoryClient.java
@@ -1,12 +1,12 @@
 package com.akentech.microservices.order.client;
 
-import com.akentech.microservices.inventory.dto.InventoryRequest;
-import com.akentech.microservices.inventory.dto.InventoryResponse;
+import com.akentech.microservices.common.dto.InventoryRequest;
+import com.akentech.microservices.common.dto.InventoryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8086/api/inventory")
+@FeignClient(name = "inventory-service", url = "http://localhost:8087/api/inventory")
 public interface InventoryClient {
 
     @PostMapping("/check")
